384536d370208e4a4d9a875e650cd33852b5b4ea
doslano-ci <integration@doslano.ru>
2026-06-10 00:57:43 +0000

    sdk: regenerate from integration spec 55f70594

---
+++ src/models/index.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Balance';
+export * from './CreateLetterRequest';
+export * from './FileSource';
+export * from './Letter';
+export * from './LetterClass';
+export * from './LetterContent';
+export * from './LetterStatus';
+export * from './ListLetters200Response';
+export * from './Party';
+export * from './PartyType';
+export * from './PaymentResult';
+export * from './Pricing';
+export * from './Problem';
+export * from './PromoResult';
+export * from './Recipient';
+export * from './RecipientInput';
+export * from './RecipientStatus';
+export * from './SenderInput';
+export * from './TrackingInfo';
+export * from './TrackingInfoEventsInner';
+export * from './WebhookEvent';
+export * from './WebhookEventData';
